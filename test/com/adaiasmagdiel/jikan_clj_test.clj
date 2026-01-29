88dba0bae0228522dcfe7b99e673ce01d6e707f9
Adaías Magdiel <adaiasmagdiell@gmail.com>
2026-01-29 17:26:04 -0300

    test: add integration test for full anime data - Add wait-a-bit helper to avoid rate limiting - Add test for fetching full anime data

--- test/com/adaiasmagdiel/jikan_clj_test.clj
+++ test/com/adaiasmagdiel/jikan_clj_test.clj
@@ -2,11 +2,21 @@
   (:require [clojure.test :refer [deftest is testing]]
             [com.adaiasmagdiel.jikan-clj :as jikan]))
 
+;; Helper to avoid rate limiting (429) during integration tests
+(defn- wait-a-bit [] (Thread/sleep 2500))
+
 (deftest anime-fetch-test
   (testing "Fetching a known anime (Cowboy Bebop)"
     (let [result (jikan/anime 1)]
       (is (not (nil? result)))
       (is (= "Cowboy Bebop" (:title result)))))
 
+  (wait-a-bit)
+  (testing "Fetching full anime data"
+    (let [result (jikan/anime 1 true)]
+      (is (not (nil? result)))
+      (is (contains? result :relations))))
+
+  (wait-a-bit)
   (testing "Fetching a non-existent anime returns nil"
     (is (nil? (jikan/anime 42)))))
