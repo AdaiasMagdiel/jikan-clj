97fc4586287b03bf90e9fd318b1d133f4d7e4632
Adaías Magdiel <adaiasmagdiell@gmail.com>
2026-01-29 17:11:26 -0300

    feat: Add anime-characters and anime-episodes functions - Update `anime-details` docstring to clarify behavior and arguments. - Add `anime-characters` function to fetch characters and staff of an anime. - Add `anime-episodes` function with optional episode argument to fetch episodes list or a single episode.

--- src/com/adaiasmagdiel/jikan_clj.clj
+++ src/com/adaiasmagdiel/jikan_clj.clj
@@ -19,13 +19,50 @@
 ;; === ANIME
 
 (defn anime
-  "Fetches anime details by ID. Returns nil if not found.
-  
-     Maps to Jikan API:
-     - false: `GET /anime/{id}`
-     - true:  `GET /anime/{id}/full`"
+  "Retrieves detailed information about a specific anime by its MyAnimeList ID.
+   Returns nil if the resource is not found.
+
+   Args:
+   - `id`: The MyAnimeList ID of the anime.
+   - `full?`: When true, fetches complete data including relations and external links.
+
+   Maps to:
+   - `GET /anime/{id}` (default)
+   - `GET /anime/{id}/full` (when full? is true)"
   ([id] (anime id false))
   ([id full?]
    (let [endpoint (str "/anime/" id (if full? "/full" ""))
+         url (str (:base-url @settings) endpoint)]
+     (utils/fetch-safe url))))
+
+(defn anime-characters
+  "Retrieves a list of characters and staff from a specific anime.
+   Returns nil if the resource is not found.
+
+   Maps to: `GET /anime/{id}/characters`"
+  [id]
+  (let [endpoint (str "/anime/" id "/characters")
+        url (str (:base-url @settings) endpoint)]
+    (utils/fetch-safe url)))
+
+(defn anime-episodes
+  "Retrieves the episode list for a specific anime or details of a single episode.
+   Returns nil if the resource is not found.
+
+   Args:
+   - `id`: The MyAnimeList ID of the anime.
+   - `episode`: (Optional) The specific episode number/ID.
+
+   Maps to:
+   - `GET /anime/{id}/episodes` (when episode is nil)
+   - `GET /anime/{id}/episodes/{episode_id}` (when episode is provided)"
+  ([id] (anime-episodes id nil))
+  ([id episode]
+   (let [base-path (str "/anime/" id "/episodes")
+         endpoint (if (nil? episode)
+                    base-path
+                    (if (number? episode)
+                      (str base-path "/" episode)
+                      (throw (IllegalArgumentException. "Episode must be a number"))))
          url (str (:base-url @settings) endpoint)]
      (utils/fetch-safe url))))
